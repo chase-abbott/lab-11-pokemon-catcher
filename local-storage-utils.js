6bc327a63016ed50acde92548ed400cc83bebe43
chase-abbott <cabbott93@gmail.com>
2021-04-05 12:03:31 -0700

    add encounterPOkemon and capturePokemon function

--- local-storage-utils.js
+++ local-storage-utils.js
@@ -1,3 +1,5 @@
+import { findById } from '.tests/utils.js'
+
 const POKEDEX = 'POKEDEX';
 
 export function getPokedex() {
@@ -15,3 +17,38 @@ export function setPokedex(parsedPokedex) {
 
     localStorage.setItem(POKEDEX, stringyPokedex);
 }
+
+export function encounterPokemon(pokemon) {
+    const pokedex = getPokedex();
+
+    const matchingPokedexItem = findById(pokemon);
+
+    if (matchingPokedexItem) {
+        matchingPokedexItem.encountered++;
+    } else {
+        const newPokedexItem = {
+            id: pokemon.id,
+            encountered: 1,
+            captured: 0
+        }
+        pokedex.push(newPokedexItem);
+    }
+    setPokedex(pokedex);
+
+    return pokedex;
+}
+
+export function capturePokemon(pokemon) {
+    const pokedex = getPokedex();
+
+    const matchingPokedexItem = findById(pokemon);
+
+    matchingPokedexItem.capturePokemon++;
+
+    setPokedex(pokedex);
+
+    return pokedex;
+}
+
+
+// - capturePokemon
