8bc00b1e21cd8ce7e984c0ebdce817c694f73929
chase-abbott <cabbott93@gmail.com>
2021-04-05 11:47:04 -0700

    add local stroage functions

---
+++ local-storage-utils.js
@@ -0,0 +1,17 @@
+const POKEDEX = 'POKEDEX';
+
+export function getPokedex() {
+    const stringyPokedex = localStorage.getItem(POKEDEX);
+
+    if (!stringyPokedex) return [];
+
+    const parsedPokedex = JSON.parse(pokedex);
+
+    return parsedPokedex;
+}
+
+export function setPokedex(parsedPokedex) {
+    const stringyPokedex = JSON.stringify(parsedPokedex);
+
+    localStorage.setItem(POKEDEX, stringyPokedex);
+}
