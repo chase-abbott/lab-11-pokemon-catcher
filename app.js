7a1dc4a1f0db00be2f2e24e2699987a8cdbe984d
chase-abbott <cabbott93@gmail.com>
2021-04-06 10:54:41 -0700

    fix stuff

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { pokeData } from './data.js';
+
 import { capturePokemon, generateThreePokemon, getPokedex } from './local-storage-utils.js'
 import { findByPokeName, findById } from './test/utils.js';
 
@@ -10,13 +10,13 @@ const img1 = document.querySelector('#poke-img-1');
 const img2 = document.querySelector('#poke-img-2');
 const img3 = document.querySelector('#poke-img-3');
 
-const pEncounter1 = document.querySelector('#encounter-1')
-const pEncounter2 = document.querySelector('#encounter-2')
-const pEncounter3 = document.querySelector('#encounter-3')
+const pEncounter1 = document.querySelector('#encounter-1');
+const pEncounter2 = document.querySelector('#encounter-2');
+const pEncounter3 = document.querySelector('#encounter-3');
 
-const pCapture1 = document.querySelector('#capture-1')
-const pCapture2 = document.querySelector('#capture-2')
-const pCapture3 = document.querySelector('#capture-3')
+const pCapture1 = document.querySelector('#capture-1');
+const pCapture2 = document.querySelector('#capture-2');
+const pCapture3 = document.querySelector('#capture-3');
 
 const button = document.querySelector('#reset-button');
 
@@ -27,9 +27,9 @@ let captures = 0;
 function createPokemonDOM() {
     const threePokemon = generateThreePokemon();
     const pokedex = getPokedex();
-    const matching1 = findById(pokedex, threePokemon[0].pokemon)
-    const matching2 = findById(pokedex, threePokemon[1].pokemon)
-    const matching3 = findById(pokedex, threePokemon[2].pokemon)
+    const matching1 = findById(pokedex, threePokemon[0].pokemon);
+    const matching2 = findById(pokedex, threePokemon[1].pokemon);
+    const matching3 = findById(pokedex, threePokemon[2].pokemon);
 
     radio1.value = threePokemon[0].pokemon;
     img1.src = threePokemon[0].url_image;
