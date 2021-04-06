06c9cab071d3a23f816f1abddd993b18a31d1efc
chase-abbott <cabbott93@gmail.com>
2021-04-05 17:24:50 -0700

    try to add stretch goals

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { pokeData } from './data.js';
-import { capturePokemon, generateThreePokemon } from './local-storage-utils.js'
-import { findByPokeName } from './test/utils.js';
+import { capturePokemon, generateThreePokemon, getPokedex } from './local-storage-utils.js'
+import { findByPokeName, findById } from './test/utils.js';
 
 const radio1 = document.querySelector('#poke-id-1');
 const radio2 = document.querySelector('#poke-id-2');
@@ -10,6 +10,10 @@ const img1 = document.querySelector('#poke-img-1');
 const img2 = document.querySelector('#poke-img-2');
 const img3 = document.querySelector('#poke-img-3');
 
+const label1 = document.querySelector('#poke-label-1');
+const label2 = document.querySelector('#poke-label-2');
+const label3 = document.querySelector('#poke-label-3');
+
 const button = document.querySelector('#reset-button');
 
 //state
@@ -18,8 +22,10 @@ let captures = 0;
 
 function createPokemonDOM() {
     const threePokemon = generateThreePokemon();
+
     radio1.value = threePokemon[0].pokemon;
     img1.src = threePokemon[0].url_image;
+
 
     radio2.value = threePokemon[1].pokemon;
     img2.src = threePokemon[1].url_image;
