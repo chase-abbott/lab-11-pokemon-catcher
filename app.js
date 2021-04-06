0463872ff0a9322d06283444ca62d32abd933c4d
chase-abbott <cabbott93@gmail.com>
2021-04-06 10:51:14 -0700

    add encounters and captures to DOM

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ const img1 = document.querySelector('#poke-img-1');
 const img2 = document.querySelector('#poke-img-2');
 const img3 = document.querySelector('#poke-img-3');
 
-const label1 = document.querySelector('#poke-label-1');
-const label2 = document.querySelector('#poke-label-2');
-const label3 = document.querySelector('#poke-label-3');
+const pEncounter1 = document.querySelector('#encounter-1')
+const pEncounter2 = document.querySelector('#encounter-2')
+const pEncounter3 = document.querySelector('#encounter-3')
+
+const pCapture1 = document.querySelector('#capture-1')
+const pCapture2 = document.querySelector('#capture-2')
+const pCapture3 = document.querySelector('#capture-3')
 
 const button = document.querySelector('#reset-button');
 
@@ -22,16 +26,25 @@ let captures = 0;
 
 function createPokemonDOM() {
     const threePokemon = generateThreePokemon();
+    const pokedex = getPokedex();
+    const matching1 = findById(pokedex, threePokemon[0].pokemon)
+    const matching2 = findById(pokedex, threePokemon[1].pokemon)
+    const matching3 = findById(pokedex, threePokemon[2].pokemon)
 
     radio1.value = threePokemon[0].pokemon;
     img1.src = threePokemon[0].url_image;
-
+    pEncounter1.textContent = `Encountered: ${matching1.encountered}`;
+    pCapture1.textContent = `Captured: ${matching1.captured}`;
 
     radio2.value = threePokemon[1].pokemon;
     img2.src = threePokemon[1].url_image;
+    pEncounter2.textContent = `Encountered: ${matching2.encountered}`;
+    pCapture2.textContent = `Captured: ${matching2.captured}`;
 
     radio3.value = threePokemon[2].pokemon;
     img3.src = threePokemon[2].url_image;
+    pEncounter3.textContent = `Encountered: ${matching3.encountered}`;
+    pCapture3.textContent = `Captured: ${matching3.captured}`;
 }
 
 createPokemonDOM();
