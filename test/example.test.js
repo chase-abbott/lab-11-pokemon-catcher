0f00c67de5f5072f797d90b06b77e4130ca5fa11
Chase Abbott <75389857+chase-abbott@users.noreply.github.com>
2021-04-05 17:30:44 -0700

    Update example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -10,7 +10,7 @@ const test = QUnit.test;
 test('Tests findbyId function', (expect) => {
     const expected = pokeData[0];
 
-    const actual = findById(pokeData, 1)
+    const actual = findById(pokeData, 1);
 
     expect.equal(actual, expected);
 });
@@ -22,14 +22,14 @@ test('Tests getPokedex function', (expect) => {
 
     const expected = JSON.parse(localStorage.getItem('POKEDEX'));
 
-    const actual = getPokedex()
+    const actual = getPokedex();
     expect.deepEqual(actual, expected);
 });
 
 test('time to test a function', (expect) => {
     const expected = pokeData[0];
 
-    const actual = findById(pokeData, '5cef3501ef6005a77cd4fd17')
+    const actual = findById(pokeData, '5cef3501ef6005a77cd4fd17');
 
     expect.equal(actual, expected);
 });
